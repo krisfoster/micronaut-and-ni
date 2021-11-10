a072e96185e8d8f9652bab2f991ebdb4dc94fab2
Oracle Public Cloud User <opc@instance-20211109-1740.subnet11091723.vcn11091723.oraclevcn.com>
2021-11-10 10:02:06 +0000

    Moved everything to the complete solution folder

---
+++ 01-create-a-mn-app/complete/triangles/src/main/java/triangles/RequestController.java
@@ -0,0 +1,21 @@
+package triangles;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import static triangles.SierpinskiTriangle.getSierpinskiTriangle;
+
+@Controller("/")
+public class RequestController {
+
+    @Get(uri="/triangle/{n}", produces=MediaType.TEXT_PLAIN)
+    public String triangle(int n) {
+        String tri = getSierpinskiTriangle(n);
+        return tri;
+    }
+
+    @Get(uri="/ping", produces=MediaType.TEXT_PLAIN)
+    public String ping() {
+        final int[] primes = Sieve.findPrimes(100000);
+        return "pong : " + primes[primes.length - 1];
+    }
+}
